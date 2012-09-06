a6b2da4ed50bc53baa81b5c229664e99eb2508d1
Elle Yoko Suzuki <elle.suzuki@gmail.com>
2012-09-05 19:21:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cubbytings::Application.config.secret_token = '088976d8ff59a75b213ae04b4c16ccc0ad50c5b19aa2ebbe206d885792ede58625d908a26056e4dfef8a2697613bc475a33e64b90bdeb76115533e3a988733d8'
